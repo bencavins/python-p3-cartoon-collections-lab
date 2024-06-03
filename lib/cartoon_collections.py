e925873bd55366a33995f35afda8fc368afd772c
Ben Cavins <ben.cavins@flatironschool.com>
2024-06-03 15:06:04 -0400

    do lab first half

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    num = 1
+    for dwarf in dwarves:
+        # print(str(num) + ".", dwarf)
+        print(f"{num}. {dwarf}")
+        num += 1  # num = num + 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    result = []
+    for call in calls:
+        new_call = call.capitalize() + "!"
+        result.append(new_call)
+    return result
 
 def long_planeteer_calls():
     pass
 
 def find_the_cheese():
     pass
+
+
+print(summon_captain_planet(["carrot", "cucumber", "pepper"]))
